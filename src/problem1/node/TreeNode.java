d5c6fbe2847d4c1198792fb18af739e32ac11feb
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-27 12:47:58 +0530

    TreeNode class created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,14 @@
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode <T>{
+    public int value;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
 }
