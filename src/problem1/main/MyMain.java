0b3d11b0223898d64276b3c530ccd5a4f93c3ce2
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-27 13:03:35 +0530

    completed class main

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,6 +6,26 @@
  */
 package problem1.main;
 // executable class
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
 
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyBinarySearchTree a = new MyBinarySearchTree();
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        System.out.println("PreOrder traversal ::");
+        a.print();
+    }
 }
