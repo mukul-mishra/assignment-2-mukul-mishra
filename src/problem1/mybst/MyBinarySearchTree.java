817b58df400963dc51f69c777b41184718cce2c2
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-27 12:54:10 +0530

    created method insert

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,5 +6,20 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+import problem1.node.TreeNode;
 public class MyBinarySearchTree {
+    public TreeNode root;
+    public int c=0;
+    public void insert(int value) {
+        TreeNode node = new TreeNode<>(value);
+        if ( root == null ) {
+            root = node;
+        }
+        else {
+            insertRec(root, node);
+        }
+    }
+
+    private void insertRec(TreeNode latestRoot, TreeNode node) {
+    }
 }
