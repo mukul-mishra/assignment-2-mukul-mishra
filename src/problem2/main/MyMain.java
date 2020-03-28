4c807d23d731065ae644bcb2f5485887299f7f4b
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-28 10:30:37 +0530

    completed main of question 2

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,8 +5,31 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
 import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyBinarySearchTree a = new MyBinarySearchTree();
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        a.insert(scanner.nextInt());
+        System.out.println("PreOrder traversal ::");
+        a.print();
+        System.out.println("PostOrder traversal ::");
+        a.printPostorder();
+    }
 }
