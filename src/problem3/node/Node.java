01caf0cab341b94fbd22a480f83a05a78f37dad1
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-28 19:56:21 +0530

    completed node class of question3

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,33 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+
+    private Student s;
+    private  Node next;
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node()
+    {
+        s=new Student();
+        next=null;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
