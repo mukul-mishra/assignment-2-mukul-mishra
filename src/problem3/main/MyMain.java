fd46f92d914973fa514838a2988b1f0a3de2a06b
dbc2201 <mukul.mishra_cs18@gla.ac.in>
2020-03-28 20:03:34 +0530

    completed main of problem3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args){
+        MyPriorityQueue m = new MyPriorityQueue();
+        for (int i = 0; i < 4; i++) {
+            Node newNode=new Node();
+            m.enqueue(newNode);
+        }
+        m.dequeue();
+    }
 }
